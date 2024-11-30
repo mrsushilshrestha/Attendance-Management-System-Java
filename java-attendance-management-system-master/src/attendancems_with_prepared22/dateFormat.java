9dc868c871c93fe969c4107cabd903e2ccb20739
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-12-01 00:37:11 +0545

    this is Java file

---
+++ java-attendance-management-system-master/src/attendancems_with_prepared22/dateFormat.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package attendancems_with_prepared22;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hp
+ */
+class dateFormat {
+
+    static Object format(Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
